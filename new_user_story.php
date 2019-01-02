4c5a1c1dad2ce0551b34df14620d452144451b9e
bryan-ym <neo424@163.com>
2019-01-02 12:11:33 +0800

    第一次提交，用于github连通测试

---
+++ new_user_story.php
@@ -0,0 +1,18 @@
+<!--User story collection-->
+
+<!DOCTYPE html> 
+<html> 
+
+<head>
+<link rel="stylesheet" type="text/css" href="css/main_bryan.css">
+</head>
+
+<body> 
+<h1>创建用户故事</h1> 
+
+<?php 
+echo "Hello World!"; 
+?> 
+
+</body> 
+</html>
