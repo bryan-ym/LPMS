434f72cf564cf54e536a4c831adf2b06a74b1669
bryan-ym <neo424@163.com>
2019-01-02 22:22:12 +0800

    实现了php式的下拉框

--- new_user_story.php
+++ new_user_story.php
@@ -1,4 +1,4 @@
-<!--User story collection-->
+<!--User story creation-->
 
 <!DOCTYPE html> 
 <html> 
@@ -9,9 +9,18 @@
 
 <body> 
 <h1>创建用户故事</h1> 
+<hr />
 
 <?php 
-echo "Hello World!"; 
+//echo "Hello World!\n"; 
+$story_roles=array("系统管理员","项目经理","PMO","项目成员","管理层","测试");
+echo "作为<select>";
+foreach ($story_roles as $value)
+{
+   print "<option value=$value>$value</option>\n";
+}
+
+echo "</select>角色";
 ?> 
 
 </body> 
